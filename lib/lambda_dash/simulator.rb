eac360e2a52fe72c88d799fef9c3b291a9e2a5ed
James Edward Gray II <james@graysoftinc.com>
2012-07-15 17:31:20 -0500

    Reading from STDIN like they told me to.

---
+++ lib/lambda_dash/simulator.rb
@@ -0,0 +1,27 @@
+module LambdaDash
+  module Simulator
+    ROOT_DIR = File.expand_path("../..", File.dirname(__FILE__))
+    
+    module_function
+    
+    def simulate(map_path)
+      reader, writer = IO.pipe
+      pid            = spawn("#{ROOT_DIR}/bin/run", in: map_path, out: writer)
+      Process.detach(pid)
+      writer.close
+      thread = Thread.new do
+        begin
+          sleep 150
+          Process.kill(:INT, pid)
+          sleep 10
+          Process.kill(:KILL, pid)
+        rescue
+          # do nothing:  our process is gone
+        end
+      end
+      reader.read
+    ensure
+      thread.kill if thread
+    end
+  end
+end
